6b5bdd136109093ef25d2b4580f0ce2a7783f222
isackmwangasu <mwangasuisack@gmail.com>
2024-04-03 12:28:06 +0300

    arrowfunctionsdone

---
+++ index.js
@@ -0,0 +1,10 @@
+// making a function to divide variables 
+const divide = (a, b) => a / b;
+divide(2000, 100)
+//squaring 
+const square = (a) => a * a;
+square(2)
+
+//adding function
+const add = (x, y) => x + y;
+add(3, 4)
